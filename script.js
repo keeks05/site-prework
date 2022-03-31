5cf53b305a9271546ed5bf3f0e67b8f6e623fee5
Glitch (glitch-blank-website) <none>
2022-03-31 01:54:00 +0000

    ⚽️🌔 Checkpoint ./script.js:43454018/966 ./index.html:43454018/55

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ var progress = 0;
 var gamePlaying = false;
 
 
+function startGame(){
+  // Create game variables
+  progress = 0;
+  gamePlaying = true;
+  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("stopBtn").classList.remove("hidden");
+}
+
+function stopGame(){
+  gamePlaying = false;
+  document.getElementById("startBtn").classList.remove("hidden");
+  document.getElementById("stopBtn").classList.add("hidden");
+}
