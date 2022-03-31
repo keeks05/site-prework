abb2e5cf7cdcf22e100faa653a75a14b49555f8e
Glitch (glitch-blank-website) <none>
2022-03-31 02:05:59 +0000

    🎬🚣 Checkpoint ./style.css:43454018/56 ./script.js:43454018/780

--- script.js
+++ script.js
@@ -4,15 +4,14 @@
 var pattern [2, 2, 4, 3, 2, 1, 2, 4];
 var progress = 0;
 var gamePlaying = false;
-
+const startBtn = document.querySelector('#startBtn');
+const stopBtn = document.querySelector('')
 
 function startGame(){
   // Create game variables
   progress = 0;
   gamePlaying = true;
-  document.getElementById("startBtn").classList.add("hidden");
-  document.getElementById("stopBtn").classList.remove("hidden");
-}
+  
 
 function stopGame(){
   gamePlaying = false;
